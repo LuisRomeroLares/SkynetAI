95f831cdaf14949db7f7467c1b9c8e2243d89aab
andreaUNAM <31602026+amoguelk@users.noreply.github.com>
2023-03-03 19:52:04 -0600

    Página de información y ayuda

--- js/app.js
+++ js/app.js
@@ -3,4 +3,10 @@ document.addEventListener("DOMContentLoaded", function () {
   var instances = M.Sidenav.init(elems);
   var elems = document.querySelectorAll(".tooltipped");
   var instances = M.Tooltip.init(elems);
+  var elems = document.querySelectorAll(".collapsible");
+  var instances = M.Collapsible.init(elems, {
+    accordion: false,
+  });
+  var elems = document.querySelectorAll(".parallax");
+  var instances = M.Parallax.init(elems);
 });
